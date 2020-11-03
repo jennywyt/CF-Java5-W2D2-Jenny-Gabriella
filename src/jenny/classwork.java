cfc3a2152865dc383a3a80708b4217e79550afe2
Jenny Wong <yintingjenny.wong@gmail.com>
2020-11-03 13:07:54 +0100

    q1 q2

--- src/jenny/classwork.java
+++ src/jenny/classwork.java
@@ -1,4 +1,38 @@
 package jenny;
 
+import java.util.*;
+
 public class classwork {
+    public static void main(String[] args) {
+
+        ArrayList<String> fruitMix = new ArrayList<String>();
+
+        fruitMix.add("Apple");
+        fruitMix.add("Pear");
+        fruitMix.add("Banana");
+        fruitMix.add("Durian");
+        fruitMix.add("Peach");
+        fruitMix.add("Blueberry");
+
+
+        ArrayList<String> vegetables = new ArrayList<String>();
+
+        vegetables.add("Cabbage");
+        vegetables.add("Onion");
+        vegetables.add("Egg Plant");
+        vegetables.add("Cucumber");
+        vegetables.add("Paprika");
+        vegetables.add("Pumpkin");
+
+        vegetables.addAll(fruitMix);
+
+        //unsorted list
+
+
+        //Sort statement
+        Collections.sort(vegetables);
+        System.out.println(vegetables);
+
+
+    }
 }
